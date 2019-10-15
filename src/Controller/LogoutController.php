1998c3c30f41c77b5ad84a6c444a07e90c62db86
sakorn.s <sakorn.s@github.com>
2019-10-15 12:50:50 +0700

    create controllers

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Logout Controller
+ *
+ *
+ * @method \App\Model\Entity\Logout[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class LogoutController extends AppController {
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index() {
+        
+    }
+
+}
---
+++ src/Controller/SettingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Setting Controller
+ *
+ *
+ * @method \App\Model\Entity\Setting[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class SettingController extends AppController {
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index() {
+        
+    }
+
+}
