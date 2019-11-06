2e4b88b9510eb2f1e8bc40aa77833b2cfa7e2156
Athiwat Wiroonphattanadate <athiwat.wir@gmail.com>
2019-11-06 16:53:47 +0700

    Update app_config.php

--- config/app_config.php
+++ config/app_config.php
@@ -1,7 +1,7 @@
 <?php 
 
 //define('SITE_URL', $_SERVER['REQUEST_SCHEME'].'://127.0.0.1/Dropbox/gold/');
-define('SITEURL', 'http://localhost:8888/Git/op-appv2/');
+define('SITEURL', 'http://orderpang-app.herokuapp.com/');
 //define('APIURL', 'https://sv.orderpang.com/');
 //define('APIURL', 'http://localhost/git/op-service/');
 define('APIURL', 'https://orderpang-service.herokuapp.com/');
